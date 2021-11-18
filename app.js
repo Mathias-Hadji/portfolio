e1d6355471914dca875a2205aa73182348d26bba
Mathias HADJI <mathias.hadji@gmail.com>
2021-11-18 14:51:32 +0100

    Debug affichage des textes du menu small screen + Lock du scroll quand le menu small screen est ouvert.

--- app.js
+++ app.js
@@ -6,7 +6,18 @@ const menuSmallScreen = document.querySelector('.menu-small-screen');
 btnMenuHamburger.addEventListener('click', () => {
 
     menuSmallScreen.classList.toggle('display-menu-small-screen');
+
+
+    // Lock scrolling when menu small screen is open
+    if(menuSmallScreen.classList.contains('display-menu-small-screen')) {
+        document.getElementsByTagName('body')[0].style.overflow = 'hidden';
+    } else {
+        document.getElementsByTagName('body')[0].style.overflow = 'visible';
+    }
 });
+
+
+
 
 
 // Animation texte au survol des liens dans le menu de navigation des petits écrans
