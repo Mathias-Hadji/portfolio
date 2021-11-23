a1a80481df1141b13db9493912a5ca7e11290d61
Mathias HADJI <mathias.hadji@gmail.com>
2021-11-23 15:11:15 +0100

    Refactoring animation du menu de navigation petit ecran.

--- app.js
+++ app.js
@@ -1,87 +1,99 @@
-
-
-// Animation menu de navigation petits écrans
+// -------------------- Animation du menu de navigation --------------------
 
 const menuHamburger = document.querySelector('.menu-hamburger');
-const iconMenuHamburger = document.querySelectorAll('.icon-menu-hamburger');
-
+const iconMenuHamburger = document.querySelector('.icon-menu-hamburger');
 const menuSmallScreen = document.querySelector('.menu-small-screen');
 
-const linkMenuSmallScreen = document.querySelectorAll('.menu-small-screen a');
+iconMenuHamburger.src = "./ressources/menu-hamburger.svg"
 
+
+// Animation du menu lorsque l'on clique sur l'icone hamburger
 menuHamburger.addEventListener('click', () => {
 
-    iconMenuHamburger.forEach(icon => {
+    // Au clic sur l'icone du menu petit écran : 
+    // - Permutation de l'image du bouton du menu hamburger en fonction de si le menu est ouvert ou fermé
+    // - Toggle la classe qui ouvre ou ferme le menu
+    // - Toggle la classe qui bloque ou débloque le scroll
 
-        icon.classList.toggle('active');
-        icon.classList.toggle('disabled');
-    });
 
+    // - Permutation de l'image du bouton du menu hamburger en fonction de si le menu est ouvert ou fermé
+    if(iconMenuHamburger.src.split('ressources')[1] === "/menu-hamburger.svg") {
+
+        iconMenuHamburger.src = "./ressources/minus-solid.svg"
+
+    } else {
+
+        iconMenuHamburger.src = "./ressources/menu-hamburger.svg"
+    }
+
+
+    // - Toggle la classe qui ouvre ou ferme le menu
+    const menuSmallScreen = document.querySelector('.menu-small-screen');
     menuSmallScreen.classList.toggle('menu-small-screen-active');
-    
-    // Lock scrolling when menu small screen is active
+
+
+    // - Toggle la classe qui bloque ou débloque le scroll
     document.getElementsByTagName('body')[0].classList.toggle('block-scroll');
-
-    linkMenuSmallScreen.forEach(link => {
-
-        link.addEventListener('click', () => {
-
-            document.getElementsByTagName('body')[0].classList.toggle('block-scroll');
-            menuSmallScreen.classList.remove('menu-small-screen-active')
-        });
-    });
 
 });
 
-// const btnMenuHamburger = document.querySelector('.menu-hamburger');
-// const menuSmallScreen = document.querySelector('.menu-small-screen');
-
-// btnMenuHamburger.addEventListener('click', () => {
-
-//     menuSmallScreen.classList.toggle('display-menu-small-screen');
 
 
-//     // Lock scrolling when menu small screen is open
-//     if(menuSmallScreen.classList.contains('display-menu-small-screen')) {
-//         document.getElementsByTagName('body')[0].style.overflow = 'hidden';
-//     } else {
-//         document.getElementsByTagName('body')[0].style.overflow = 'visible';
-//     }
-// });
-
-
-
-
-
-// Animation texte au survol des liens dans le menu de navigation des petits écrans
-
+// Animations du menu aux evenements sur les liens du menu petit ecran
+const linkMenuSmallScreen = document.querySelectorAll('.menu-small-screen a');
 const txtMenuHover = document.querySelector('.txt-menu-hover');
-const liMenuSmallScreen = document.querySelectorAll('.menu-small-screen a');
 
-liMenuSmallScreen.forEach(link => {
+linkMenuSmallScreen.forEach(link => {
+
+    link.addEventListener('click', () => {
+
+        // Au clic sur les liens : 
+        // - Fermeture du menu
+        // - Remise en place de l'image du bouton du menu hamburger position par défaut (menu fermé).
+        // - Débloquage du scroll 
+
+
+        // - Fermeture du menu
+        menuSmallScreen.classList.remove('menu-small-screen-active');
+
+        // - Remise en place de l'image du bouton du menu hamburger position par défaut (menu fermé).
+        iconMenuHamburger.src = "./ressources/menu-hamburger.svg"
+
+        // - Débloquage du scroll 
+        document.getElementsByTagName('body')[0].classList.remove('block-scroll');
+    });
+
 
     link.addEventListener('mouseover', (e) => {
 
+        // Au survol sur les liens :
+        // - Injection innerHTML du texte survolé par l'utilisateur
         txtMenuHover.innerHTML = `${e.target.innerHTML}`;
     });
+
 });
 
 
-
-// Animation navbar bg darker on scroll
+// Animation bg de la navbar plus opaque au scroll
 const menuDesktop = document.querySelector('.menu-desktop');
 
 window.addEventListener('scroll', () => {
 
-    if(window.scrollY >= 150 && !menuDesktop.classList.contains('menu-desktop-bg-darker')){
+    // Au scroll : 
+    // - Si la position Y du scroll passe au dessus de 150 ==> ajout de la classe pour rendre le bg plus opaque
+    // - Sinon la position du scroll est inférieur à Y ==> suppression de la classe pour le rendre plus opaque
 
-        menuDesktop.classList.remove('menu-desktop-bg-lighter');
+
+    if(window.scrollY >= 150) {
+
+        // - Si la position Y du scroll passe au dessus de 150 ==> ajout de la classe pour rendre le bg plus opaque
         menuDesktop.classList.add('menu-desktop-bg-darker');
 
-    } else if(window.scrollY <= 150 && !menuDesktop.classList.contains('menu-desktop-bg-lighter')){
-        
+    } else {
+
+        // - Sinon la position du scroll est inférieur à Y ==> suppression de la classe pour rendre le bg plus opaque
         menuDesktop.classList.remove('menu-desktop-bg-darker');
-        menuDesktop.classList.add('menu-desktop-bg-lighter');
+
     }
 });
 
